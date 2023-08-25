7445bc7afb6c34919911f3e8e5048c03a5ea99d3
AkelaDumindu <ikadthilakshana@std.appsc.sab.ac.lk>
2023-08-25 13:45:46 +0530

    adding new cart functions and their related things

--- functions/common_function.php
+++ functions/common_function.php
@@ -32,7 +32,8 @@ function getproducts() {
                         <div class='card-body'>
                             <h5 class='card-title'>$product_title</h5>
                             <p class='card-text'>$product_description</p>
-                            <a href='#' class='btn btn-p'>Add to cart</a>
+                            <p class='card-text'>Price: $product_price/-</p>
+                            <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a>
                             <a href='product_details.php?product_ID=$product_id' class='btn-v '>View more</a>
                         </div>
                     </div>
@@ -78,7 +79,8 @@ function get_setect_category() {
                     <div class='card-body'>
                         <h5 class='card-title'>$product_title</h5>
                         <p class='card-text'>$product_description</p>
-                        <a href='#' class='btn btn-p'>Add to cart</a>
+                        <p class='card-text'>Price: $product_price/-</p>
+                      <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a>
                         <a href='product_details.php?product_id=$product_id' class='btn-v '>View more</a>
                     </div>
                 </div>
@@ -123,7 +125,8 @@ function get_setect_brand() {
                     <div class='card-body'>
                         <h5 class='card-title'>$product_title</h5>
                         <p class='card-text'>$product_description</p>
-                        <a href='#' class='btn btn-p'>Add to cart</a>
+                        <p class='card-text'>Price: $product_price/-</p>
+                        <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a>
                         <a href='product_details.php?product_id=$product_id' class='btn-v '>View more</a>
                     </div>
                 </div>
@@ -195,7 +198,7 @@ function search_product() {
             // count the num of rows resent in the database
             $num_of_rows = mysqli_num_rows($result_query);
             if($num_of_rows == 0) {
-                echo "<h3 class='text-center'>No matching serch results</h3>";
+                echo "<h3 class='text-center'>No matching search results</h3>";
             }
 
             while($row = mysqli_fetch_assoc($result_query)) {
@@ -214,7 +217,8 @@ function search_product() {
                         <div class='card-body'>
                             <h5 class='card-title'>$product_title</h5>
                             <p class='card-text'>$product_description</p>
-                            <a href='#' class='btn btn-p'>Add to cart</a>
+                            <p class='card-text'>Price: $product_price/-</p>
+                             <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a>
                             <a href='product_details.php?product_id=$product_id' class='btn-v '>View more</a>
                         </div>
                     </div>
@@ -253,8 +257,9 @@ function get_all_products() {
                         <div class='card-body'>
                             <h5 class='card-title'>$product_title</h5>
                             <p class='card-text'>$product_description</p>
-                            <a href='#' class='btn btn-p'>Add to cart</a>
-                            <a href='product_details.php?product_id=$product_id' class='btn-v '>View more</a>
+                            <p class='card-text'>Price: $product_price/-</p>
+                             <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a>
+                            <a href='index.php' class='btn-v '>Go Home</a>
                         </div>
                     </div>
                 </div>";
@@ -297,7 +302,8 @@ function view_details() {
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
-                                <a href='#' class='btn btn-p'>Add to cart</a>
+                                <p class='card-text'>Price: $product_price/-</p>
+                                 <a href='index.php?add_to_cart= $product_id' class='btn btn-p'>Add to cart</a> 
                                 <a href='index.php' class='btn-v '>Back to home</a>
                             </div>
                         </div>
@@ -320,4 +326,105 @@ function view_details() {
     }
 
 }
+
+
+
+
+
+//***************************************************************
+//get the ip address
+
+function getIPAddress() {
+    //whether ip is from the share internet
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    //whether ip is from the proxy
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+//whether ip is from the remote address
+    else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+//$ip = getIPAddress();
+//echo 'User Real IP Address - '.$ip;
+
+
+
+//*****cart function
+    function cart(){
+        if(isset($_GET['add_to_cart'])){
+            global $con;
+            $get_ip_add = getIPAddress();
+            $get_product_id = $_GET['add_to_cart'];
+            $select_query = "SELECT * FROM cart_details WHERE ip_address = '$get_ip_add' AND product_id =  $get_product_id";
+            $result_query = mysqli_query($con, $select_query);
+            $num_of_rows = mysqli_num_rows($result_query);
+            if($num_of_rows > 0) {
+                echo "<script>alert('This Item is already present inside the cart')</script>";
+                echo "<script>window.open('index.php', '_self')</script>";
+            }else{
+                $insert_query = "INSERT INTO cart_details (product_id, ip_address, quantity) VALUES ($get_product_id, '$get_ip_add', 0)";
+                $result_query = mysqli_query($con, $insert_query);
+                echo "<script>alert('Item is added to cart')</script>";
+                echo "<script>window.open('index.php', '_self')</script>";
+            }
+
+        }
+    }
+
+
+//get cart item numbers
+
+function cart_item(){
+    if(isset($_GET['add_to_cart'])) {
+        global $con;
+        $get_ip_add = getIPAddress();
+        $select_query = "SELECT * FROM cart_details WHERE ip_address = '$get_ip_add'";
+        $result_query = mysqli_query($con, $select_query);
+        $count_cart_item = mysqli_num_rows($result_query);
+    }else{
+        global $con;
+        $get_ip_add = getIPAddress();
+        $select_query = "SELECT * FROM cart_details WHERE ip_address = '$get_ip_add'";
+        $result_query = mysqli_query($con, $select_query);
+        $count_cart_item = mysqli_num_rows($result_query);
+
+    }
+    echo $count_cart_item;
+
+}
+
+//Total price function
+function total_cart_price(){
+    global $con;
+    $total_price=0;
+//    first choose record, related to the ip address
+    $get_ip_add = getIPAddress();
+    $cart_query = "SELECT * FROM cart_details WHERE ip_address = '$get_ip_add'";
+    $result = mysqli_query($con, $cart_query);
+//    next find all product related to the product id in product table
+    while ($row = mysqli_fetch_array($result)){
+        $product_id = $row['product_id'];
+        $select_product = "SELECT * FROM products WHERE product_id = $product_id";
+        $result_products = mysqli_query($con, $select_product);
+//        next find all prices for related product
+        while ($row_product = mysqli_fetch_array($result_products)){
+            $product_price = array($row_product['product_price']);  /*[200,300]*/
+            $product_values = array_sum($product_price);   /*  [500]  */
+            $total_price += $product_values;
+
+
+        }
+    }
+    echo $total_price;
+}
+
+
+
+
+
 ?>
